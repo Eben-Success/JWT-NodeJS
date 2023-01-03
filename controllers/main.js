804501be9c60e9bbb2e75e31785cec095e25f8c6
Eben-Success <aebenezer237@gmail.com>
2023-01-03 21:29:15 +0000

    update

---
+++ controllers/main.js
@@ -0,0 +1,16 @@
+
+
+const login = (req, res) =>{
+    res.send('Fake Login/Register/Signup Route')
+}
+
+const dashboard = async (req, res) =>{
+    const luckyNumber = Math.floor(Math.random() * 100)
+    res.status(200).json({msg:`Hello, John Doe`, secret: `Here is your authorizeed data, your lucky
+    number is ${luckyNumber}`})
+}
+
+module.exports = {
+    login,
+    dashboard
+}
